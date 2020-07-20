a81e22a4eaa07e581ab3e75640e071c3f14c4900
tatyanasb <btatyana45@gmail.com>
2020-07-19 23:32:35 -0400

    started clean up of the unfunctioning modal mess. Reverted js to working code. Downloaded new potential bcg imgs for all pages

--- app.js
+++ app.js
@@ -1,67 +1,11 @@
-//all buttons
-const btn = document.querySelectorAll('.modal-button'); //check code
+const aboutBtn = document.querySelector('.about-btn');
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close-btn');
 
-//all modals
-const modals = document.querySelectorAll('.modal');
+aboutBtn.addEventListener('click', function(){
+    modal.classList.add('open-modal');
+});
 
-//close
-const close = document.getElementsByClassName("close-btn");
-
-//click & open modal
-for (var i = 0; i < btn.length; i++){
-    btn[i].onclick = function(e) {
-        e.preventDefault();
-        modal = document.querySelector(e.target.getAttribute("href"));
-        modal.style.display = "block";
-    }
-}
-
-//close modal
-for (var i = 0; i < close.length; i++){
-    close[i].onclick = function(){
-        for (var index in modals){
-            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
-        }
-    }
-}
-// window.onclick = function(event) {
-//     if (event.target.classList.contains('modal')) {
-//      for (var index in modals) {
-//       if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
-//      }
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-// const aboutBtn = document.querySelector('.about-btn');
-// const modal = document.querySelector('.modal');
-// const closeBtn = document.querySelector('.close-btn');
-
-// aboutBtn.addEventListener('click', function(){
-//     modal.classList.add('open-modal');
-// });
-
-// closeBtn.addEventListener('click', function(){
-//     modal.classList.remove('open-modal');
-
-
-// const containers = document.querySelectorAll('.overlay-container');
-
-// containers.forEach(function(container){
-//     // console.log(container);
-//     const closeBtn = container.querySelector('.close-btn');
-//     // console.log(closeBtn);
-//     closeBtn.addEventListener('click', function(item) {
-//         if (item !== container) {
-//             item.classList.remove('.open-overlay');
-//         }
-//     })
-//  });
+closeBtn.addEventListener('click', function(){
+    modal.classList.remove('open-modal');
+});
